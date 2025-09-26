a9e32a2997313a1c82673463a11a756ff677815d
Santosh Adabala <santosh.adabala91@gmail.com>
2025-09-26 14:06:57 +0530

    Add files via upload

---
+++ Fetch.ps1
@@ -0,0 +1,8 @@
+﻿Write-Output "Fetching all resource groups"
+Write-Output "****************************"
+Get-AzResourceGroup
+Write-Output "****************************"
+Write-Output "Fetching all storage account"
+Write-Output "****************************"
+Get-AzStorageAccount
+Write-Output "****************************"
