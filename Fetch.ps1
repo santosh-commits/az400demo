dd0edf5af60e3b29533dc17c71fec7616474cecf
Santosh Adabala <santosh.adabala91@gmail.com>
2025-09-26 14:16:34 +0530

    Delete Fetch.ps1

--- Fetch.ps1
+++
@@ -1,8 +0,0 @@
-﻿Write-Output "Fetching all resource groups"
-Write-Output "****************************"
-Get-AzResourceGroup
-Write-Output "****************************"
-Write-Output "Fetching all storage account"
-Write-Output "****************************"
-Get-AzStorageAccount
-Write-Output "****************************"
